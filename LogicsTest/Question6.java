c481bc81938fc526825f17e8db62209b9a2d769f
pablo-conte <pablo.correa.nr@gmail.com>
2022-12-05 19:24:23 -0300

    Complete -> Question 6 Done!

---
+++ LogicsTest/Question6.java
@@ -0,0 +1,27 @@
+// 6)Escreva um programa EM JAVA que lê três números reais, calcule e apresente a soma e a média dos números informados com 2 casas depois da vírgula.
+import java.util.Scanner;
+
+public class Question6 {
+    
+    public static void main(String[] args) {
+        
+        Scanner monitor = new Scanner(System.in);
+        
+        System.out.print("Escreva o primeiro numero real: ");
+        double n1 = monitor.nextDouble();
+
+        System.out.print("Escreva o segundo numero real: ");
+        double n2 = monitor.nextDouble();
+
+        System.out.print("Escreva o terceiro numero real: ");
+        double n3 = monitor.nextDouble();
+
+
+        double soma = n1 + n2 + n3;
+        double media = soma / 3;
+
+        System.out.printf("%f + %f + %f = %.2f \n", n1, n2, n3, soma);
+        System.out.printf("A média entre esses 3 numeros é: %.2f\n", media);
+
+    }
+}
